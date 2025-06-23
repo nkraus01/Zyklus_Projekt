7cec3434bf33fe7995b3272cb898ef00ff11830d
nkraus01 <nathalie_kraus@t-online.de>
2025-06-23 16:32:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -739,8 +739,8 @@ with chiara:
         ax.legend(fontsize=5)
 
         ax.set_ylim(36.2, 37.2)  # feste Achsenskalierung
-        ax.tick_params(axis='x', labelsize=6)
-        ax.tick_params(axis='y', labelsize=5)
+        ax.tick_params(axis='x', labelsize=4)
+        ax.tick_params(axis='y', labelsize=4)
         plt.xticks(rotation=45)
         st.pyplot(fig)
 
