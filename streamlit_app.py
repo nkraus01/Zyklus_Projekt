b5d16ebc009518b274bf127be2778407786721a7
nkraus01 <nathalie_kraus@t-online.de>
2025-06-24 09:09:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ with start:
     
     with spalte2:
         st.subheader("Überblick")
+        st.write("")
         st.write("Mit Hilfe von Chiaras Code kannst du dir abhängig von deiner Körpertemperaturen deinen Eisprung ausrechnen lassen und das graphisch darstellen.")
         st.info("Klicke dafür oben auf 'Zyklus und Temperatur'!")
         st.write(" ")
