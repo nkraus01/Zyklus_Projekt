401f3479cc220c346f564492703e9dd665a37f4c
nkraus01 <nathalie_kraus@t-online.de>
2025-06-26 15:36:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ with start:
     st.write("Wir sind drei Studentinnen und haben im Rahmen einer Projektarbeit diese Website erstellt.")
     st.write("Hier findest du viele interessante Facts und Darstellungen rund um den weiblichen Zyklus im Allgemeinen sowie ganz individuell auf dich abgestimmt.")
     st.write("""Wir möchten betonen, dass unsere Website **keinen Anspruch auf Richtigkeit** hat und Fehler enthalten kann! Daher überprüfe die Informationen auf jeden Fall, 
-    bevor du sie verwendest. Sie dient der Darstellung und groben Informationsbereitstellung.""")
+    bevor du sie verwendest.""")
 
     st.subheader(" ")
     
